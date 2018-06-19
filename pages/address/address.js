be1a509e777420527e1179ffc5e7cbea7551e77a
SourceOnline <1803723001@qq.com>
2018-06-19 18:14:24 +0800

    619
    619

--- pages/address/address.js
+++ pages/address/address.js
@@ -1,24 +1,26 @@
 // pages/address/address.js
+//获取应用实例
+var app = getApp()
+// 引入SDK核心类
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.js');
+var qqmapsdk;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    switch1: true
   },
 
-  onChange(event) {
-    const detail = event.detail;
-    this.setData({
-      'switch1': detail.value
-    })
-
-  },
 
   bindAdd:function(){
     var that = this
     that.chooseLocation(null)
+  },
+
+  test:function(e){
+    var id = e.currentTarget.dataset.aid;
+    console.log(id)
   },
 
   //地图选择位置
@@ -30,16 +32,16 @@ Page({
       success: function (res) {
         // success
         console.log(res)
-        that.setData({
-          hasLocation: true,
-          location: {
-            longitude: res.longitude,
-            latitude: res.latitude
-          }
-        })
-        app.location.longitude = res.longitude
-        app.location.latitude = res.latitude
-        that.getCity();
+        // that.setData({
+        //   hasLocation: true,
+        //   location: {
+        //     longitude: res.longitude,
+        //     latitude: res.latitude,
+        //     name: res.name,
+        //     address: res.address
+        //   }
+        // })
+        that.setHome(res)
       },
       fail: function () {
         // fail
@@ -50,11 +52,87 @@ Page({
     })
   },
 
+  //处理数据-上传家位置
+  setHome: function (location){
+    var HOST = app.globalData.URL_PATH;
+    var token = app.globalData.token;
+    var that = this;
+    wx.request({
+      url: HOST + '/api/address/setHome',
+      data: {
+        longitude: location.longitude,
+        latitude: location.latitude,
+        message: location.address,
+        token: token
+      },
+      method: "GET",
+      complete: function (res) {
+        console.log(res.data)
+        if (res == null || res.data == null) {
+          reject(new Error('网络请求失败'))
+        }
+      },
+      success: function (res) {
+        if (res.data.status == 1) {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'success',
+            duration: 2000
+          })
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'error',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+
+  //获取存储位置列表
+  getList: function () {
+    var HOST = app.globalData.URL_PATH;
+    var token = app.globalData.token;
+    var that = this;
+    wx.request({
+      url: HOST + '/api/address/getList',
+      data: {
+        token: token
+      },
+      method: "GET",
+      complete: function (res) {
+        console.log(res.data)
+        if (res == null || res.data == null) {
+          reject(new Error('网络请求失败'))
+        }
+      },
+      success: function (res) {
+        if (res.data.status == 1) {
+          that.setData({
+            addressList: res.data.data.address
+          })
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'error',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    qqmapsdk = new QQMapWX({
+      key: app.QQMapWXKey // 必填
+    });
+    var that = this
+    that.getList()
   },
 
   /**
